aefc67c585772fa39c34b2755af3bf09b58dbab8
Melanye Guadalupe Ramos Munoz <melanye.ramos2312@alumnos.udg.mx>
2026-09-14 22:59:34 -0600

    Agregar visualizacion de datos

---
+++ visualizacion.py
@@ -0,0 +1,17 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+
+# Cargar los datos
+datos = np.loadtxt("datos.csv", delimiter=",", skiprows=1)
+
+edad = datos[:, 0]
+ingreso = datos[:, 1]
+
+# Crear gráfico
+plt.scatter(edad, ingreso)
+
+plt.xlabel("Edad")
+plt.ylabel("Ingreso")
+plt.title("Relación entre edad e ingreso")
+
+plt.show()
