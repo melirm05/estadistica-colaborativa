66f50459b03351f9dd992ddda28cd80de2a6c3dc
Melanye Guadalupe Ramos Munoz <melanye.ramos2312@alumnos.udg.mx>
2026-09-14 22:42:19 -0600

    Agregar calculo de mediana

--- estadistica.py
+++ estadistica.py
@@ -10,5 +10,11 @@ ingreso = datos[:, 1]
 media_edad = np.mean(edad)
 media_ingreso = np.mean(ingreso)
 
+# Calcular mediana
+mediana_edad = np.median(edad)
+mediana_ingreso = np.median(ingreso)
+
 print("Media de edad:", media_edad)
 print("Media de ingreso:", media_ingreso)
+print("Mediana de edad:", mediana_edad)
+print("Mediana de ingreso:", mediana_ingreso)
